ed90faa8d617a66bf710ee25e8b23a161ccd3b31
Automated Release Deployment <yannick.wieland@hs-pforzheim.de>
2024-12-06 07:32:25 +0000

    auto update release

--- src/scripts/utils/matomoHelper.js
+++ src/scripts/utils/matomoHelper.js
@@ -116,7 +116,7 @@ function loadMatomo() {
     (function() {
         var u="//matomo.simplipfy.org/";
         _paq.push(["setTrackerUrl", u+"matomo.php"]);
-        _paq.push(["setSiteId", "2"]);
+        _paq.push(["setSiteId", "3"]);
         var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0];
         g.async=true; g.src=u+"matomo.js"; s.parentNode.insertBefore(g,s);
     })();
